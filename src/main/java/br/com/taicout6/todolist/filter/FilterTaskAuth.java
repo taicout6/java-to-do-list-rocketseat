4f93a706b8f3db9114ee2d303f01fbb51ca25d8a
Taina Coutinho <taicout6@gmail.com>
2023-10-11 17:02:23 -0300

    Validação de usuario e senha. Obs: Falta fazer com que não afete todas as rotas da aplicação

--- src/main/java/br/com/taicout6/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/taicout6/todolist/filter/FilterTaskAuth.java
@@ -1,10 +1,14 @@
 package br.com.taicout6.todolist.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.taicout6.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,23 +17,48 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
 
+  @Autowired
+  private IUserRepository userRepository;
+
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
 
         // Pegar auth (usuario e senha)
         var authorizathion = request.getHeader("Authorization");
-        System.out.println("Authorization");
-        System.out.println(authorizathion);
 
+        var userAuthEncoded = authorizathion.substring("Basic".length()).trim();
+        System.out.println("Authorization");
+        System.out.println(userAuthEncoded);
+
+        byte[] userAuthDecoded = Base64.getDecoder().decode(userAuthEncoded);
+        System.out.println(userAuthDecoded);
+
+        var userAuthString = new String(userAuthDecoded);
+        System.out.println(userAuthString);
+
+        // ["username", "password"]
+        String[] credentials = userAuthString.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
+        System.out.println(username);
+        System.out.println(password);
 
         // Validar usuario
+        var user = this.userRepository.findByUsername(username);
+        if (user == null) {
+          response.sendError(401);
+        } else {
+          // Validar senha
+          var passwordVerified = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+          if (passwordVerified.verified) {
+            filterChain.doFilter(request, response);
+          } else {
+            response.sendError(401);
+          }
 
-        // Validar senha
-
-        // Segue viagem
-
-        filterChain.doFilter(request, response);
+          // Segue viagem
+        }
   }
 
 }
