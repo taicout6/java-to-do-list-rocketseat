daed3572f24e5174228c9ac18bce561ea4050bea
Taina Coutinho <taicout6@gmail.com>
2023-10-11 13:57:55 -0300

    Implementando metodo POST para cadastro de tarefas no TaskController

--- src/main/java/br/com/taicout6/todolist/task/TaskController.java
+++ src/main/java/br/com/taicout6/todolist/task/TaskController.java
@@ -1,5 +1,8 @@
 package br.com.taicout6.todolist.task;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/tasks")
 public class TaskController {
   
+  @Autowired
+  private ITaskRepository taskRepository;
+
+  @PostMapping("/")
+  public TaskModel create(@RequestBody TaskModel taskModel) {
+    var task = this.taskRepository.save(taskModel);
+    return task;
+  }
 }
