68afefea3fba1edb45558dab7929e8304ae41068
Taina Coutinho <taicout6@gmail.com>
2023-10-13 14:40:21 -0300

    Atualizando metodo de update (Os dados anteriores agora não serão perdidos)

--- src/main/java/br/com/taicout6/todolist/task/TaskController.java
+++ src/main/java/br/com/taicout6/todolist/task/TaskController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.taicout6.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -52,9 +53,15 @@ public class TaskController {
 
   @PutMapping("/{id}")
   public TaskModel update(@RequestBody TaskModel  taskModel, HttpServletRequest request, @PathVariable UUID id) {
-    var idUser = request.getAttribute("idUser");
-    taskModel.setIdUser((UUID) idUser);
-    taskModel.setId(id);
-    return this.taskRepository.save(taskModel);
+    // var idUser = request.getAttribute("idUser");
+
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    Utils.copyNonNullProperties(taskModel, task);
+
+    // taskModel.setIdUser((UUID) idUser);
+    // taskModel.setId(id);
+
+    return this.taskRepository.save(task);
   }
 }
--- src/main/java/br/com/taicout6/todolist/utils/Utils.java
+++ src/main/java/br/com/taicout6/todolist/utils/Utils.java
@@ -9,11 +9,11 @@ import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
 
 public class Utils {
-  public void copyNonNullProperties(Object source, Object target) {
+  public static void copyNonNullProperties(Object source, Object target) {
     BeanUtils.copyProperties(source, target, getNullPropertyNames(source));
   }
 
-  public String[] getNullPropertyNames(Object source) {
+  public static String[] getNullPropertyNames(Object source) {
     final BeanWrapper src = new BeanWrapperImpl(source);
 
     PropertyDescriptor[] pds = src.getPropertyDescriptors();
