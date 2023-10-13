b03f42a6f5f1dec6392f41049de47c6e512c2084
Taina Coutinho <taicout6@gmail.com>
2023-10-13 14:08:05 -0300

    Add metodo PUT para atualizar uma tarefa

--- src/main/java/br/com/taicout6/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/taicout6/todolist/filter/FilterTaskAuth.java
@@ -26,7 +26,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
 
         var servletPath = request.getServletPath();
 
-        if (servletPath.equals("/tasks/")) {
+        if (servletPath.startsWith("/tasks/")) {
           // Pegar auth (usuario e senha)
           var authorizathion = request.getHeader("Authorization");
 
